83f997dcdbfbf2ccb07ff1a5c54ac399fe6d1147
hkl1014 <linghangking93@hotmail.com>
2017-09-10 01:25:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars =["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(a,b){
+  a.shift(b)
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+  a=a.shift(b)
+}
